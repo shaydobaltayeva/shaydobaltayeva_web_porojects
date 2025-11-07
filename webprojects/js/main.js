8b9ab2927b35c4c8ffc06b65631d829b098378bb
shaydobaltayeva <shaydobaltayeva593@gmail.com>
2025-11-07 11:12:46 +0500

    Yangi fayllar qoshildi

---
+++ webprojects/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+    var cnt = 0;
+
+    var row = null;
+    $('#adddata').click(function () {
+        if($(this).text()=="Qo'shish"){
+            const ism= $("#ism").val().trim();
+            const familiya = $("#familiya").val().trim();
+        }
+
+
+
+
+    });
+
+
+});
---
+++ webprojects/js/tashqiscript.js
@@ -0,0 +1,13 @@
+
+function tugmabosilganda() {
+
+    alert("salom")
+    // const bir = document.getElementById('birinchison').value;
+    // const ikki = document.getElementById('ikkinchison').value;
+    //
+    //
+    // var natija = parseInt(bir)+parseInt(ikki);
+    //
+    // document.getElementById("natija").innerText ="Natija = "+natija;
+}
+
